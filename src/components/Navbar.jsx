e826d562f0090a92a31762a00dabd08936a03ac7
wild-dino <daritie8@gmail.com>
2022-03-13 17:05:27 +0300

    import/export

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a>Главная</a>
+      </div>
+      <div>
+        <a>Мой профиль</a>
+      </div>
+      <div>
+        <a>Каталог</a>
+      </div>
+      <div>
+        <a>Блог</a>
+      </div>
+      <div>
+        <a>Пункты приема</a>
+      </div>
+      <div>
+        <a>Галерея</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
