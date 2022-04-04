49e1d9b15d7fe8b3dea2246bf4c747a87bd1f412
wild-dino <daritie8@gmail.com>
2022-04-04 20:20:50 +0300

    add counting items in cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,18 @@ import logo from './../../assets/Icons/InEcology.svg'
 import cart from './../../assets/Icons/cart.svg'
 
 const Header = () => {
-  return (
-    <header className={s.header}>
-      <img className={s.logo} src={logo} />
-      <div className={s.avatar}><img src="https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg" alt="" /></div>
-      <img className={s.cart} src={cart} />
-    </header>
-  );
+    return (
+        <header className={s.header}>
+            <img className={s.logo} src={logo}/>
+            <div className={s.avatar}><img
+                src="https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg" alt=""/>
+            </div>
+            <div className={s.cart}>
+                <img src={cart}/>
+                <div className={s.count}>0</div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
