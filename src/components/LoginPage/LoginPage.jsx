2a40e3c6c8918f57251571eb893e2de2ced040dc
wild-dino <daritie8@gmail.com>
2022-04-28 22:14:39 +0300

    created new register form

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,19 @@
-import {NavLink,  useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 import {useDispatch} from "react-redux";
 import {Form} from "../Form/Form";
 import {setUser} from "../../redux/auth-reducer";
 import s from "./LoginPage.module.css";
+import {useState} from "react";
 
 const LoginPage = () => {
+    const [state, setState] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = state;
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -20,14 +28,43 @@ const LoginPage = () => {
             .catch(console.error)
     }
 
+    const handleSubmit = () => {
+    }
+    const handleChange = () => {
+    }
+
     return (
         <div>
             <h2 className={s.title}>Добро пожаловать!</h2>
-            <Form
-                title="Войти"
-                handleClick={handleLogin}
-            />
-            <div>или <NavLink to="/register">Зарегестрируйтесь</NavLink></div>
+            {/*<Form*/}
+            {/*    title="Войти"*/}
+            {/*    handleClick={handleLogin}*/}
+            {/*/>*/}
+            <form className={s.formSignin} onSubmit={handleSubmit}>
+                <h3>Войдите</h3>
+                <input
+                    type="email"
+                    id="inputEmail"
+                    className="form-control"
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={handleChange}
+                    value={email}
+                    required
+                />
+                <input
+                    type="password"
+                    id="inputPassword"
+                    className="form-control2"
+                    placeholder="Password"
+                    name="email"
+                    onChange={handleChange}
+                    value={password}
+                    required
+                />
+                <button className={s.btn} type="submit">Войти</button>
+            </form>
+            <div>Нет аккаунта? <NavLink to="/register">Зарегестрируйтесь</NavLink></div>
         </div>
     );
 };
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import {NavLink, useNavigate} from "react-router-dom";
 import {Form} from "../Form/Form";
 import {useDispatch} from "react-redux";
 import {setUser} from "../../redux/auth-reducer";
+import s from "../LoginPage/LoginPage.module.css";
 
 const RegisterPage = () => {
+    const [state, setState] = useState({
+        displayName: "",
+        email: "",
+        password: "",
+        passwordConfirm: ""
+    });
+
+    const {email, password, displayName, passwordConfirm} = state;
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const handleSubmit = () => {
+    }
+    const handleChange = () => {
+    }
 
     const handleRegister = (email, password) => {
         const auth = getAuth();
@@ -23,11 +37,54 @@ const RegisterPage = () => {
     return (
         <div>
             <h2>Регистрация</h2>
-            <Form
-                title="Register"
-                handleClick={handleRegister}
-            />
-            <div>Есть аккаунт? <NavLink to="/login">Войдите</NavLink></div>
+            {/*<Form*/}
+            {/*    title="Register"*/}
+            {/*    handleClick={handleRegister}*/}
+            {/*/>*/}
+            <form className={s.formSignin} onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    id="displayName"
+                    className="form-control"
+                    placeholder="Имя Фамилия"
+                    name="displayName"
+                    onChange={handleChange}
+                    value={displayName}
+                    required
+                />
+                <input
+                    type="email"
+                    id="inputEmail"
+                    className="form-control"
+                    placeholder="Электронная почта"
+                    name="email"
+                    onChange={handleChange}
+                    value={email}
+                    required
+                />
+                <input
+                    type="password"
+                    id="inputPassword"
+                    className="form-control2"
+                    placeholder="Пароль"
+                    name="password"
+                    onChange={handleChange}
+                    value={password}
+                    required
+                />
+                <input
+                    type="password"
+                    id="inputPasswordConfirm"
+                    className="form-control2"
+                    placeholder="Повторите пароль"
+                    name="passwordConfirm"
+                    onChange={handleChange}
+                    value={passwordConfirm}
+                    required
+                />
+                <button className={s.btn} type="submit">Регистрация</button>
+            </form>
+            <div><NavLink to="/login"> Назад </NavLink></div>
         </div>
     );
 };
